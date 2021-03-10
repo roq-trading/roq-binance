0379fc2438a7b31c5bbbcba74917d9804470ab42
thraneh <thraneh@gmail.com>
2021-03-10 11:52:24 +0100

    ensure all usage of flags (using abseil-cpp) will compile with the default standard version

---
+++ src/roq/binance/flags.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/binance/flags/flags.h"
+
+namespace roq {
+namespace binance {
+
+using flags::Flags;
+
+}  // namespace binance
+}  // namespace roq
