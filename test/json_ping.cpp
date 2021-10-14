8c420b3405f17ee9b7b502609e02f802ff4e8c72
thraneh <thraneh@gmail.com>
2021-10-14 05:36:56 +0200

    adding some tests

---
+++ test/json_ping.cpp
@@ -0,0 +1,20 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#include <gtest/gtest.h>
+
+#include "roq/core/json/parser.h"
+
+#include "roq/binance/json/ping.h"
+
+using namespace roq;
+using namespace roq::binance;
+
+using namespace std::chrono_literals;
+
+TEST(json_ping, simple) {
+  auto message = R"({)"
+                 R"("serverTime":1634180186435)"
+                 R"(})";
+  auto obj = core::json::Parser::create<json::Ping>(message);
+  EXPECT_EQ(obj.server_time, 1634180186435ms);
+}
