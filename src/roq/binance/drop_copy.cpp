a52ee8f66513d891e631f95192af32dde3c9901b
thraneh <thraneh@gmail.com>
2023-03-05 05:05:53 +0100

    adding TradeUpdate.user

--- src/roq/binance/drop_copy.cpp
+++ src/roq/binance/drop_copy.cpp
@@ -314,7 +314,7 @@ void DropCopy::operator()(Trace<json::ExecutionReport> const &event) {
                 .fills = {&fill, 1},
                 .routing_id = order.routing_id,
                 .update_type = {},
-                .user = {},
+                .user = shared_.get_user_name(order.user_id),
             };
             create_trace_and_dispatch(handler_, trace_info, trade_update, true, order.user_id);
           }
