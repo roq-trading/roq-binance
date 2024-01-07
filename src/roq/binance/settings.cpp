c091ed5cafa61291fad356f252fd8680d6e1a1bc
thraneh <thraneh@gmail.com>
2024-01-07 06:17:14 +0100

    always log flags

--- src/roq/binance/settings.cpp
+++ src/roq/binance/settings.cpp
@@ -13,7 +13,7 @@ Settings::Settings(args::Parser const &args)
     : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER}, flags::Flags{flags::Flags::create()},
       common{flags::Common::create()}, rest{flags::REST::create()}, ws{flags::WS::create()},
       ws_api_2{flags::WS_API::create()} {
-  log::debug("settings={}"sv, *this);
+  log::info("settings={}"sv, *this);
 }
 
 }  // namespace binance
