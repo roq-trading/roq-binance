8ad14a69640fb7b3249dfcdad7211a25b7309396
thraneh <thraneh@gmail.com>
2026-06-28 02:46:14 +0000

    moving fix_bridge into roq-server

--- src/roq/binance/fix_bridge/main.cpp
+++ src/roq/binance/fix_bridge/main.cpp
@@ -14,7 +14,7 @@ using namespace std::literals;
 
 namespace {
 auto const INFO = roq::Service::Info{
-    .description = "Roq's Binance futures gateway (FIX interface)"sv,
+    .description = "Roq's Binance gateway (FIX interface)"sv,
     .package_name = ROQ_PACKAGE_NAME,
     .build_version = ROQ_BUILD_VERSION,
     .build_number = ROQ_BUILD_NUMBER,
