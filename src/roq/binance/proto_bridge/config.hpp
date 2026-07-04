ac60fcbdcd01c96027ff157b1bd7691ca8140aa6
thraneh <thraneh@gmail.com>
2026-07-04 03:18:56 +0000

    preparing roq-*-proto-bridge

---
+++ src/roq/binance/proto_bridge/config.hpp
@@ -0,0 +1,15 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/binance/gateway/config.hpp"
+
+namespace roq {
+namespace binance {
+namespace proto_bridge {
+
+using Config = gateway::Config;
+
+}  // namespace proto_bridge
+}  // namespace binance
+}  // namespace roq
