86bf7f1b4c8a9179856da6f216307e5683584639
thraneh <thraneh@gmail.com>
2026-07-09 15:09:49 +0000

    rename *-bridge (from *-proto-bridge)

--- src/roq/binance/bridge/config.hpp
+++ src/roq/binance/bridge/config.hpp
@@ -6,10 +6,10 @@
 
 namespace roq {
 namespace binance {
-namespace proto_bridge {
+namespace bridge {
 
 using Config = gateway::Config;
 
-}  // namespace proto_bridge
+}  // namespace bridge
 }  // namespace binance
 }  // namespace roq
--- src/roq/binance/bridge/main.cpp
+++ src/roq/binance/bridge/main.cpp
@@ -6,7 +6,7 @@
 
 #include "roq/logging/flags/settings.hpp"
 
-#include "roq/binance/proto_bridge/application.hpp"
+#include "roq/binance/bridge/application.hpp"
 
 using namespace std::literals;
 
@@ -28,5 +28,5 @@ auto const INFO = roq::Service::Info{
 int main(int argc, char **argv) {
   roq::flags::Args args{argc, argv, INFO.description, INFO.build_version};
   roq::logging::flags::Settings settings{args};
-  return roq::binance::proto_bridge::Application{args, settings, INFO}.run();
+  return roq::binance::bridge::Application{args, settings, INFO}.run();
 }
